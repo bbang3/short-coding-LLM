7aca889f6353714ade35fc5651ca0d9c07a6b024
Hwang Sangmin <hwsmin920@gmail.com>
2023-12-24 22:29:25 +0900

    Dobby is Free!

--- pass@k_eval/Main.py
+++ pass@k_eval/Main.py
@@ -1,11 +1,16 @@
 import evaluation
 import os
+import argparse
 
 if __name__=="__main__":
     in_dir = "predictions"
     out_dir = "evaluations"
 
-    experiment_name = "summary_1"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment", type=str, default="baseline")
+    args = parser.parse_args()
+    
+    experiment_name = args.experiment
     in_path = os.path.join(in_dir, f"prediction_{experiment_name}.jsonl")
     out_path = os.path.join(out_dir, f"evaluation_{experiment_name}")
     evaluation.evaluate(samplePath=in_path, to_json=True, json_name=out_path, add_error_list=True)
